b29e1600b4fa09ace7e122b65b71cfcc03924148
Sardius <0xsardius@gmail.com>
2025-05-22 21:00:03 -0400

    helperConfig and fixing deploy scripts

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+// Deploy mocks when on a local anvil chain
+// Otherwise, grab the existing address from the live network
+}
